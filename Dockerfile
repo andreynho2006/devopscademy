e5c1d0a28c2c6e818ea92d5ac1a83bc49475f664
andreicirlan <andreynho2006@gmail.com>
2022-11-07 22:55:42 +0200

    Homework from module 2 and 3

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+from ubuntu:latest
+COPY tema.sh /
+CMD ["/bin/bash", "/tema.sh"]
---
+++ tema.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# display current user
+echo "Current user is: $USER"
+
+# display current working directory
+echo "Current working directory is: $(pwd)"
+
+# display all files in current directory
+echo -e "Files in current directory are: \n$(ls)"
+
+# display current date
+echo "Current date is: $(date)"
